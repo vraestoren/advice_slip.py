1315008dc749b978286728e8a0740ba5bf7f37b3
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 15:31:04 +0300

    Update advice_slip.py

--- src/advice_slip.py
+++ src/advice_slip.py
@@ -1,23 +1,19 @@
-from requests import get
+from requests import Session
 
 class AdviceSlip:
 	def __init__(self) -> None:
 		self.api = "https://api.adviceslip.com"
-		self.headers = {
+		self.session = Session()
+		self.session.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
 	def get_random_advice(self) -> dict:
-		return get(
-			f"{self.api}/advice"
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/advice").json()
 
 	def get_advice_by_id(self, slip_id: int) -> dict:
-		return get(
-			f"{self.api}/advice/{slip_id}"
-			headers=self.headers).json()
+		return self.session.get(f"{self.api}/advice/{slip_id}").json()
 
 	def search_advice(self, query: str) -> dict:
-		return get(
-			f"{self.api}/advice/search/{query}",
-			headers=self.headers).json()
+		return self.session.get(
+			f"{self.api}/advice/search/{query}").json()
